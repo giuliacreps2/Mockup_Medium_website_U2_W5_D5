5c6effb83fa35d084e646ae9408c9c6d791239a3
Giulia Creps <giulia.creps2@gmail.com>
2026-01-12 10:31:29 +0100

    fixed details

--- index.js
+++ index.js
@@ -35,13 +35,13 @@ document.addEventListener("scroll", function () {
   console.log("Scroll attuale:", window.scrollY);
 
   if (window.scrollY > fineHero) {
-    header.style.position = "fixed";
+    /*header.style.position = "fixed";
     header.style.top = "0";
     header.style.left = "0";
     header.style.right = "0";
-    header.style.zIndex = "999";
+    header.style.zIndex = "999";*/
     header.style.backgroundColor = "#ffffff";
-    header.style.transition = "background-color 0.4s ease";
+    //header.style.transition = "background-color 0.4s ease";
     button.style.backgroundColor = "#1a8917";
     button.style.transition = "background-color 0.4s ease";
   } else {
